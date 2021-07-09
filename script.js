43d0613bc0b0bd1561554ad6adfc628115129a4c
GowthamRaj K <43011442+gowthamrajk@users.noreply.github.com>
2021-07-09 13:12:56 +0530

    Create script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Web Developer", "Design Thinker", "UI Designer"],
+        strings: ["Full Stack Web Developer", "Design Thinker", "UI Designer", "Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
