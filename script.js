262fd370f7aad45dc199221f4f8df2ce75b0ee1a
GowthamRaj K <43011442+gowthamrajk@users.noreply.github.com>
2021-07-09 13:13:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Design Thinker", "Student Placement Coordinator", "Student Alumini Coordinator", "Ex-President - Technical Clubs"],
+        strings: ["Software Developer", "Full Stack Web Developer", "Design Thinker", "Student Placement Coordinator", "Student Alumini Coordinator", "Ex-President - Technical Clubs"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
